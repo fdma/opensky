dd332f1d4ee026e2c26a09de774780e54b570094
Asanta2019 <f.d.m.a@mail.ru>
2022-01-13 23:15:27 +0500

    Функция кнопки[починил]

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,12 @@
 
 void setup() {
   Serial.begin(9600);
-  pinMode(3, INPUT_PULLUP);
-}
-
-void loop() {
-  bool btnFlag = false;
-  // сделать перебор с индексом, что бы не было говнокода
-  bool btnState1 = !digitalRead(3);
-  bool btnState2 = !digitalRead(4);
-  bool btnState3 = !digitalRead(5);
-  button(btnFlag, btnState1);
+  pinMode(2, INPUT_PULLUP);
 }
 
 // в идеале запихать в отдельный файл как класс
-void button(bool btnState, bool btnFlag) {
+void button(bool btnState) {
+  bool btnFlag = false;
   if(btnState && !btnFlag) {
     btnFlag = true;
     Serial.println("pressed");
@@ -24,4 +16,12 @@ void button(bool btnState, bool btnFlag) {
     btnFlag = false;
     Serial.println("unpressed");
   } 
+}
+
+void loop() {
+  // сделать перебор с индексом, что бы не было говнокода
+  bool btnState1 = digitalRead(2);
+  bool btnState2 = digitalRead(4);
+  bool btnState3 = digitalRead(5);
+  button(btnState1);
 }
