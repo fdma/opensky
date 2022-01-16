b6872b80bd63e4be807d4c7f993f3016a1dc5633
Asanta2019 <f.d.m.a@mail.ru>
2022-01-16 22:38:14 +0500

    Добавлена функция светодиода и пищалки

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,41 @@
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(11, OUTPUT);
 }
 
 // в идеале запихать в отдельный файл как класс
-void button(bool btnState) {
+bool button(bool btnState) {
   bool btnFlag = false;
   if(btnState && !btnFlag) {
-    btnFlag = true;
-    Serial.println("pressed");
+    return btnFlag = true;
   } 
   else {
-    btnFlag = false;
-    Serial.println("unpressed");
+    return btnFlag = false;
   } 
+}
+
+void buzzer(bool buzzerState) {
+  if(buzzerState == true) {
+    tone(12, 2000, 50);
+  }
+}
+
+void diod(bool diodState) {
+  if(diodState == true) {
+    digitalWrite(11, HIGH);
+  }
+  else {
+    digitalWrite(11, LOW);
+  }
 }
 
 void loop() {
   // сделать перебор с индексом, что бы не было говнокода
   bool btnState1 = digitalRead(2);
-  bool btnState2 = digitalRead(4);
-  bool btnState3 = digitalRead(5);
-  button(btnState1);
+  bool btnState2 = digitalRead(3);
+  bool btnState3 = digitalRead(4);
+  buzzer(button(btnState1));
+  diod(button(btnState2));
 }
